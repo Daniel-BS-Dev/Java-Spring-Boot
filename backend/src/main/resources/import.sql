e5d5de8385bea9bf7b49f1f558b12a2e1db73a08
Daniel-BS-Dev <danielbenedito355@gmail.com>
2021-11-17 16:14:15 -0300

    seed

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -3,3 +3,16 @@ INSERT INTO tb_category (name) VALUES ('Carros');
 INSERT INTO tb_category (name) VALUES ('Roupas');
 INSERT INTO tb_category (name) VALUES ('Acesorios');
 INSERT INTO tb_category (name) VALUES ('Calças');
+
+
+INSERT INTO tb_product (name, price) VALUES ('Civic', 90.000);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (1,2);
+
+INSERT INTO tb_product (name, price) VALUES ('Fiat', 59.000);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (2,2);
+
+INSERT INTO tb_product (name, price) VALUES ('Toro', 89.000);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (3,2);
+
+INSERT INTO tb_product (name, price) VALUES ('Corola', 109.000);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (4,2);
