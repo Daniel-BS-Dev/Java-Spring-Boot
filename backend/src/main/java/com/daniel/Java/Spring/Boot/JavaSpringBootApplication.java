0797bbd69fb6ae7216a9f32da2c7d404f0778f12
Daniel-BS-Dev <danielbenedito355@gmail.com>
2021-11-17 12:56:46 -0300

    First commit

---
+++ backend/src/main/java/com/daniel/Java/Spring/Boot/JavaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.daniel.Java.Spring.Boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringBootApplication.class, args);
+	}
+
+}
