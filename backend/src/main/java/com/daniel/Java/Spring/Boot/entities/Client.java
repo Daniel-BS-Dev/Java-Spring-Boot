e6b2c95f7a9198712b305127143fb320d6b6af00
Daniel-BS-Dev <danielbenedito355@gmail.com>
2021-11-18 09:37:31 -0300

    Class client and address

---
+++ backend/src/main/java/com/daniel/Java/Spring/Boot/entities/Client.java
@@ -0,0 +1,111 @@
+package com.daniel.Java.Spring.Boot.entities;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import com.daniel.Java.Spring.Boot.enums.TypeClient;
+
+@Entity
+@Table(name="tb_client")
+public class Client implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String name;
+	private String email;
+	private String cpfOrCnpj;
+	private TypeClient type;
+	
+	
+	public Client() {
+		
+	}
+	
+	public Client(Integer id, String name, String email, String cpfOrCnpj, TypeClient type) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.cpfOrCnpj = cpfOrCnpj;
+		this.type = type;
+	}
+
+
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCpfOrCnpj() {
+		return cpfOrCnpj;
+	}
+
+	public void setCpfOrCnpj(String cpfOrCnpj) {
+		this.cpfOrCnpj = cpfOrCnpj;
+	}
+
+	public TypeClient getType() {
+		return type;
+	}
+
+	public void setType(TypeClient type) {
+		this.type = type;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Client other = (Client) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+
+}
--- backend/src/main/java/com/daniel/Java/Spring/Boot/enums/TypeClient.java
+++ backend/src/main/java/com/daniel/Java/Spring/Boot/enums/TypeClient.java
@@ -1,5 +1,47 @@
 package com.daniel.Java.Spring.Boot.enums;
 
-public class TypeClient {
+public enum TypeClient {
+	
+	PESSOAFISICA(1, "Pessoa Fisica"),
+	PESSOAJURIDICA(2, "Pessoa Juridica");
+	
+	private Integer code; 
+	private String name;
+	
+	private TypeClient(Integer code, String name) {
+		this.code = code;
+		this.name = name;
+	}
+
+	public Integer getCode() {
+		return code;
+	}
+
+	public void setCode(Integer code) {
+		this.code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public static TypeClient toEnum(Integer code) {
+		if(code == null) {
+			return null;
+		}
+		
+		for(TypeClient x: TypeClient.values()) {
+			if(code.equals(x.getCode())) {
+				return x;
+			}
+		}
+		
+		throw new IllegalArgumentException("Id invalido"+ code);
+	}
+	
 
 }
