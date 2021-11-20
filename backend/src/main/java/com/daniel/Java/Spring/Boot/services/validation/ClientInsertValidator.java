a364f18fc95e3fbb9598a975124ed1d072afd9b1
Daniel-BS-Dev <danielbenedito355@gmail.com>
2021-11-20 14:12:49 -0300

    Email no-repeat

--- backend/src/main/java/com/daniel/Java/Spring/Boot/services/validation/ClientInsertValidator.java
+++ backend/src/main/java/com/daniel/Java/Spring/Boot/services/validation/ClientInsertValidator.java
@@ -6,14 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.daniel.Java.Spring.Boot.dto.ClientInsertDTO;
+import com.daniel.Java.Spring.Boot.entities.Client;
 import com.daniel.Java.Spring.Boot.enums.TypeClient;
+import com.daniel.Java.Spring.Boot.repositories.ClientRepository;
 import com.daniel.Java.Spring.Boot.resources.exceptions.FieldMessage;
 import com.daniel.Java.Spring.Boot.services.validation.utils.ValidationCpfOrCnpj;
 
 public class ClientInsertValidator implements ConstraintValidator<ClientInsertValid, ClientInsertDTO> {
-
 	
+	@Autowired
+	private ClientRepository rep;
+
 	@Override
 	public void initialize(ClientInsertValid ann) {
 	}
@@ -32,6 +38,13 @@ public class ClientInsertValidator implements ConstraintValidator<ClientInsertVa
 		
 		if(dto.getType().equals(TypeClient.PESSOAJURIDICA.getCode()) && !ValidationCpfOrCnpj.isValidCNPJ(dto.getCpfOrCnpj())) {
 			list.add(new FieldMessage("CpfOrCnpj" , "CNPJ invalido"));
+		}
+		
+		Client client = rep.findByEmail(dto.getEmail());
+		
+		if(client != null) {
+			list.add(new FieldMessage("Email:", "Email ja existe"));
+			
 		}
 		
 		for (FieldMessage e : list) {
